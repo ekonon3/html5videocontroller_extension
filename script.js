22bbcbfab41ca84954fc67646fd4ccfa781e6a13
ekonon3 <eli.kononovich@gmail.com>
2025-09-14 22:30:34 -0400

    add picture in picture button

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ console.log('Loading HTML5 Video Controller extension');
 const videoPlayerCollection = document.getElementsByTagName('video');
 
 // Functions to control video
-function playPause(videoPlayer)
-{
+function playPause(videoPlayer) {
 	if(videoPlayer.paused) {
 		videoPlayer.play();
 	} else {
@@ -15,14 +14,12 @@ function playPause(videoPlayer)
 	}
 }
 
-function rewind(videoPlayer, time)
-{
+function rewind(videoPlayer, time) {
 	videoPlayer.currentTime -= time;
 	console.log('Play position: ' + videoPlayer.currentTime);
 }
 
-function fastForward(videoPlayer, time)
-{
+function fastForward(videoPlayer, time) {
 	console.log(videoPlayer.currentTime);
 	console.log(time);
 	console.log(videoPlayer.currentTime += time);
@@ -30,50 +27,52 @@ function fastForward(videoPlayer, time)
 	console.log('Play position: ' + videoPlayer.currentTime);
 }
 
-function goToStart(videoPlayer)
-{
+function goToStart(videoPlayer) {
 	videoPlayer.currentTime = 0;
 	console.log('Play position: ' + videoPlayer.currentTime);
 }
 
-function goToEnd(videoPlayer)
-{
+function goToEnd(videoPlayer) {
 	//videoPlayer.currentTime = videoPlayer.duration;
 	videoPlayer.currentTime += 9999;
 	console.log('Play position: ' + videoPlayer.currentTime);
 }
 
-function increasePlaybackSpeed(videoPlayer, speed)
-{
+function increasePlaybackSpeed(videoPlayer, speed) {
 	videoPlayer.playbackRate += speed;
 	console.log('Playback speed: ' + videoPlayer.playbackRate + 'x');
 	playbackSpeed = videoPlayer.playbackRate;
 	sendPlaybackRate();
 }	
 
-function decreasePlaybackSpeed(videoPlayer, speed)
-{
+function decreasePlaybackSpeed(videoPlayer, speed) {
 	videoPlayer.playbackRate -= speed;
 	console.log('Playback speed: ' + videoPlayer.playbackRate + 'x');
 	playbackSpeed = videoPlayer.playbackRate;
 	sendPlaybackRate();
 }
 
-function setNormalPlaybackSpeed(videoPlayer)
-{
+function setNormalPlaybackSpeed(videoPlayer) {
 	videoPlayer.playbackRate = 1;
+	console.log('Playback speed: ' + videoPlayer.playbackRate + 'x');
 	playbackSpeed = videoPlayer.playbackRate;
 	sendPlaybackRate();
 }
 
-function getPlaybackRate(videoPlayer)
-{
+function getPlaybackRate(videoPlayer) {
 	playbackSpeed = videoPlayer.playbackRate;
 }
 
-function sendPlaybackRate()
-{
+function sendPlaybackRate() {
 	chrome.runtime.sendMessage({ type: "playback-rate", value: playbackSpeed });
+}
+
+function togglePictureInPicture(videoPlayer) {
+  if (document.pictureInPictureElement) {
+    document.exitPictureInPicture();
+  } else {
+    videoPlayer.requestPictureInPicture();
+  }
 }
 
 // Handle messages and commands
@@ -96,12 +95,12 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 			timeConfig = parseInt(msg.value);
 			return;
 		case "get-playbackrate":
-		/*
+			/*
 			for (v of videoPlayerCollection)
 			{
 				getPlaybackRate(v);
 			}
-		*/
+			*/	
 			sendPlaybackRate();
 			return;
 		default:
@@ -111,51 +110,48 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
 
 	switch(command) {
 		case "playBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				playPause(v);
 			}
 			break;
 		case "fastFwdBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				fastForward(v, timeConfig);
 			}
 			break;
 		case "rewindBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				rewind(v, timeConfig);
 			}
 			break;
 		case "goToStartBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				goToStart(v);
 			}
 			break;
 		case "goToEndBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				goToEnd(v);
 			}
 			break;
 		case "fasterBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				increasePlaybackSpeed(v, speedRateConfig);
 			}
 			break;
 		case "slowerBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				decreasePlaybackSpeed(v, speedRateConfig);
 			}
 			break;
 		case "normalSpeedBtn":
-			for (v of videoPlayerCollection)
-			{
+			for (v of videoPlayerCollection) {
 				setNormalPlaybackSpeed(v);
+			}
+			break;
+		case "togglePiPBtn":
+			for (v of videoPlayerCollection) {
+				togglePictureInPicture(v);
 			}
 			break;
 		default:

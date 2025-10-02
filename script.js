001120d9b9cb0dddb5d3854e35abd1fe4383f23b
ekonon3 <eli.kononovich@gmail.com>
2025-10-01 20:27:30 -0400

    Update timeout time for manual video selection from 30 seconds to 15 seconds

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ function selectVideo() {
 		video.addEventListener('mouseout', this);
 		video.addEventListener('click', this);
 		video.addEventListener('contextmenu', this);
-		const timer = setTimeout(removeSelectListeners, 30000, video);
+		const timer = setTimeout(removeSelectListeners, 15000, video);
 		timeoutArray.push(timer);
 	}
 }
